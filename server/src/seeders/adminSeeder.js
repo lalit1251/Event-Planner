cd3e7e76e04e65c5b875445aa19d0eac5e6e19af
lalit1251 <lalitmuskan5@gmail.com>
2025-07-25 19:26:46 +0530

    data of admin seeder

--- server/src/seeders/adminSeeder.js
+++ server/src/seeders/adminSeeder.js
@@ -10,3 +10,21 @@ const seederAdmin = async ()=>{
 
 
 
+const adminPassword = await bcryt.hash("Admin@123",10);
+const AdminUser = {
+    name: "Admin User",
+    email: "admin@mywebsite.com",
+    number:"9691973978",
+    photo:"https://placehold.co/600x400?text=A",
+    gender:"Male",
+    representing : "N/A",
+    occupation: "N/A",
+    address: "N/A",
+    city: "N/A",
+    state: "Active",
+    district: "N/A",
+    role:"Admin",
+}
+
+
+
